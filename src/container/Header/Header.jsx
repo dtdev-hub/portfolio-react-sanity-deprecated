b0f5152b81b4d25b120916f7586984304474dc10
Do Tung <54771496+dt-japanscript@users.noreply.github.com>
2022-03-01 16:32:48 +0900

    Bugfix for typo of app__flex

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -18,14 +18,14 @@ const scaleVariants = {
 
 const Header = () => {
     return (
-        <div id='home' className='app__header app_flex'>
+        <div id='home' className='app__header app__flex'>
             <motion.div
                 whileInView={{ x: [-100, 0], opacity: [0, 1] }}
                 transition={{ duration: 0.5 }}
                 className='app__header-info'
             >
                 <div className="app__header-badge">
-                    <div className="badge-cmp app_flex">
+                    <div className="badge-cmp app__flex">
                         <span>👋</span>
                         <div style={{ marginLeft: 20 }}>
                             <p className='p-text'>Hello, I am</p>
@@ -61,7 +61,7 @@ const Header = () => {
             >
                 {[images.flutter, images.redux, images.sass].map((circle, index) => (
                     <div className='circle-cmp app__flex' key={`circle-${index}`}>
-                        <img src={circle} alt='circle' />
+                        <img src={circle} alt='profile_bg' />
                     </div>
                 ))}
             </motion.div>
@@ -69,4 +69,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header;
