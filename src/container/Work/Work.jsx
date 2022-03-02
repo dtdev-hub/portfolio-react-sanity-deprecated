389c76e09851f0cac8314a06c804045b78494e52
Do Tung <54771496+dt-japanscript@users.noreply.github.com>
2022-03-02 11:44:06 +0900

    Create app__skills-list

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -3,7 +3,7 @@ import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
 import { AppWrap } from '../../wrapper';
-import { urlFor, clent, client } from '../../client';
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
